47272260e0d3ae2a2b7958e6c4e06e4a9c9eb694
Claire Savage <clairejsavage@gmail.com>
2016-01-29 19:44:51 -0800

    saves js, css, html changes

---
+++ starter_code/js/main.js
@@ -0,0 +1,83 @@
+console.log("main.js loaded!");
+
+
+// //MODEL: Data model******
+
+// //the data that underlies each "move" of the game…
+
+// //1 player; 20 questions/potential correct;
+// //3 incorrect answers; 4 winner categories
+
+// // var incorrectAns = 0
+// // var correctAns = 0
+// // var player = 0
+// // var winningCategory1 = incorrectAns < 3 && correctAns > 15 && correctAns < 20;
+// // var winningCategory2 = inCorrectAns >= 3 && correctAns < 15 && correctAns > 10;
+// // var winningCategory3 = inCorrectAns >= 3 && correctAns < 10 && correctAns > 5;
+// // var winningCategory4 = incorrectAns >= 3 && correctAns < 5 && correctAns > 0;
+
+// //winning conditions…
+
+// // if inCorrectAns != 3 && correctAns <
+
+
+
+// //game states or modes…
+
+
+
+// //the process of rendering:
+
+
+// //when will you render?
+
+
+// //when you render, what do you need to know?
+
+
+// //how will you render? What structures might ease that?
+
+
+
+// // Global Variables
+
+
+
+// //State of the board
+
+
+
+// //State of the game
+
+
+// //Console helper: Print the state!
+
+
+// /*MODEL: Game Behavior**********
+
+
+
+
+// /*Helper Methods*******************
+
+// //Screen render
+
+
+// /* VIEW: Render******************
+
+
+
+// /*Interaction: User-initiated handlers*********
+
+
+// /*Interaction: Other gameplay functions******
+
+
+
+// /* Start up **********
+
+// //Global variables
+
+// //DOM functions
+
+// //Start the game!
